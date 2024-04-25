b03224627a616bc82c62ae32d20f91f265605b94
Slayer-Wolf <jhammer228@gmail.com>
2024-04-25 16:27:34 +0530

    made new changes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,53 @@
-import Card from "./Card.js";
 import { useEffect, useState } from "react";
-import "./App.css";
-export default function App() {
-	const [Countries, setCountries] = useState([]);
+//import "./styles.css";
 
-	const fetchFlag = async () => {
-		try {
-			const response = await fetch("https://restcountries.com/v3.1/all");
-			if (!response.ok) {
-				throw new Error("Failed to fetch data");
-			}
-			const data = await response.json();
-			return data;
-		} catch (error) {
-			console.error("Error fetching data:", error);
-		}
-	};
+export default function App() {
+	const [countries, setCountries] = useState([]);
 
 	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const data = await fetchFlag();
-				setCountries(data);
-			} catch (error) {
-				console.error("Error fetching data:", error);
-			}
-		};
-
-		fetchData();
+		fetch("https://restcountries.com/v3.1/all")
+			.then((res) => res.json())
+			.then((data) => setCountries(data))
+			.catch((err) => console.error("Error fetching data:", err));
 	}, []);
 
+	const cardStyle = {
+		width: "200px",
+		border: "1px solid #ccc",
+		borderRadius: "10px",
+		margin: "10px",
+		padding: "10px",
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		justifyContent: "center",
+	};
+
+	const imageStyle = {
+		width: "100px",
+		height: "100px",
+	};
+
+	const containerStyle = {
+		display: "flex",
+		flexWrap: "wrap",
+		justifyContent: "center",
+		alignItems: "ceter",
+		height: "100vh",
+	};
+
 	return (
-		<>
-			<div className="gridBox">
-				{Countries?.map((flagData, index) => (
-					<Card key={index} flag={flagData} />
-				))}
-			</div>
-		</>
+		<div style={containerStyle}>
+			{countries.map((country) => (
+				<div key={country.cca3} style={cardStyle}>
+					<img
+						src={country.flags.png}
+						alt={`Flag of ${country.name.common}`}
+						style={imageStyle}
+					/>
+					<h2>{country.name.common}</h2>
+				</div>
+			))}
+		</div>
 	);
 }
