cf2713508b1ffd41075b50590800ec7ffb3903d3
Slayer-Wolf <jhammer228@gmail.com>
2024-04-25 16:20:19 +0530

    made few changes

--- src/App.js
+++ src/App.js
@@ -4,24 +4,30 @@ import "./App.css";
 export default function App() {
 	const [Countries, setCountries] = useState([]);
 
-	// const fetchFlag = async () => {
-	// 	try {
-	// 		const response = await fetch("https://restcountries.com/v3.1/all");
-	// 		if (!response.ok) {
-	// 			throw new Error("Failed to fetch data");
-	// 		}
-	// 		const data = await response.json();
-	// 		return data;
-	// 	} catch (error) {
-	// 		console.error("Error fetching data:", error);
-	// 	}
-	// };
+	const fetchFlag = async () => {
+		try {
+			const response = await fetch("https://restcountries.com/v3.1/all");
+			if (!response.ok) {
+				throw new Error("Failed to fetch data");
+			}
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			console.error("Error fetching data:", error);
+		}
+	};
 
 	useEffect(() => {
-		fetch("https://restcountries.com/v3.1/all")
-			.then((res) => res.json())
-			.then((data) => setCountries(data))
-			.catch((err) => console.error("Error fetching data", err));
+		const fetchData = async () => {
+			try {
+				const data = await fetchFlag();
+				setCountries(data);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+			}
+		};
+
+		fetchData();
 	}, []);
 
 	return (
