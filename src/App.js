81c4540ffc679e803eb12f54fc428cf226570271
Slayer-Wolf <jhammer228@gmail.com>
2024-04-29 16:52:47 +0530

    pushed code for coutry

--- src/App.js
+++ src/App.js
@@ -2,52 +2,52 @@ import { useEffect, useState } from "react";
 //import "./styles.css";
 
 export default function App() {
-	const [countries, setCountries] = useState([]);
+  const [countries, setCountries] = useState([]);
 
-	useEffect(() => {
-		fetch("https://restcountries.com/v3.1/all")
-			.then((res) => res.json())
-			.then((data) => setCountries(data))
-			.catch((err) => console.error("Error fetching data:", err));
-	}, []);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+      .catch((err) => console.error("Error fetching data:", err));
+  }, []);
 
-	const cardStyle = {
-		width: "200px",
-		border: "1px solid #ccc",
-		borderRadius: "10px",
-		margin: "10px",
-		padding: "10px",
-		display: "flex",
-		flexDirection: "column",
-		alignItems: "center",
-		justifyContent: "center",
-	};
+  const cardStyle = {
+    width: "200px",
+    border: "1px solid #ccc",
+    borderRadius: "10px",
+    margin: "10px",
+    padding: "10px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  };
 
-	const imageStyle = {
-		width: "100px",
-		height: "100px",
-	};
+  const imageStyle = {
+    width: "100px",
+    height: "100px",
+  };
 
-	const containerStyle = {
-		display: "flex",
-		flexWrap: "wrap",
-		justifyContent: "center",
-		alignItems: "ceter",
-		height: "100vh",
-	};
+  const containerStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "ceter",
+    height: "100vh",
+  };
 
-	return (
-		<div style={containerStyle}>
-			{countries.map((country) => (
-				<div key={country.cca3} style={cardStyle}>
-					<img
-						src={country.flags.png}
-						alt={`Flag of ${country.name.common}`}
-						style={imageStyle}
-					/>
-					<h2>{country.name.common}</h2>
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div style={containerStyle}>
+      {countries.map((country) => (
+        <div key={country.cca3} style={cardStyle}>
+          <img
+            src={country.flags.png}
+            alt={`Flag of ${country.name.common}`}
+            style={imageStyle}
+          />
+          <h2>{country.name.common}</h2>
+        </div>
+      ))}
+    </div>
+  );
 }
